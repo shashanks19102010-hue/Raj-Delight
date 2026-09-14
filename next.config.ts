4858dad76b88a8b37379110cebee0d4d8a1ca678
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-09-14 17:38:16 +0530

    chore: tighten image remote policy

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com' },
-      { protocol: 'https', hostname: 'img3.restaurantguru.com' },
-      { protocol: 'https', hostname: 'img02.restaurantguru.com' },
-      { protocol: 'https', hostname: 'content.jdmagicbox.com' },
     ],
   },
 };
