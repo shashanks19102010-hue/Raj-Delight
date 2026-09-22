fbed79129336900dedf0933f5e2cca88190317cd
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-09-22 15:08:02 +0530

    feat: mount menu price enhancer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { MenuPriceEnhancer } from '@/components/MenuPriceEnhancer';
 import { RajDelightExperienceV10 } from '@/components/RajDelightExperienceV10Real';
 
 export default function Page() {
-  return <RajDelightExperienceV10 />;
+  return (
+    <>
+      <RajDelightExperienceV10 />
+      <MenuPriceEnhancer />
+    </>
+  );
 }
